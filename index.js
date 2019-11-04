1f7972463cc4e163fddea2826262cc2b545b9fa4
Alex Duval <alexduval71@gmail.com>
2019-11-04 16:02:51 +0100

    set ParticlesJS

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('loaded');
+// particleJS
+
+import particlesJS from 'particles.js/particles';
+const particles = window.particlesJS;
+import particlesConfig from './particlesjs-config.json';
+particles('bg', particlesConfig);
+
+//
